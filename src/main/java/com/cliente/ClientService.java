2f7496c14f92853f1a666d7448ab59c93502ef47
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2025-02-04 08:26:41 -0300

    Create DTO, Repository and empty Service

---
+++ src/main/java/com/cliente/ClientService.java
@@ -0,0 +1,12 @@
+package com.cliente;
+
+import com.cliente.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+}
